50cd4806dd1afc248145d4681008d5fbf866c071
Siddhant P. Pardeshi <siddhantpp@gmail.com>
2024-09-30 23:44:23 +0530

    Add public-samples/podcast-marketing-automation-saas-zqmsmg/src/api/apps/marketing/__init__.py

---
+++ src/api/apps/marketing/__init__.py
@@ -0,0 +1,5 @@
+# It allows Django to recognize this directory as an app and import it in the project settings.
+
+# If needed in the future, you can add any necessary initialization code or imports here.
+# For example:
+# default_app_config = 'api.apps.marketing.apps.MarketingConfig'
