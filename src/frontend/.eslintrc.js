14019d4aa20534a0d5a83dd1bece0be3fcf78419
Siddhant P. Pardeshi <siddhantpp@gmail.com>
2024-09-30 23:48:12 +0530

    Add public-samples/podcast-marketing-automation-saas-zqmsmg/src/frontend/.eslintrc.js

---
+++ src/frontend/.eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+  ],
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'react-hooks',
+    'jsx-a11y',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+  // Human tasks
+  // TODO: Review and adjust ESLint rules based on team coding standards
+  // TODO: Ensure all necessary ESLint plugins are installed in package.json
+};
