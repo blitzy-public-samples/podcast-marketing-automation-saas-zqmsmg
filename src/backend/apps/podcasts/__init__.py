33c114387ee8788f60905c4a438f894ea5aa745e
Siddhant P. Pardeshi <siddhantpp@gmail.com>
2024-09-30 23:46:18 +0530

    Add public-samples/podcast-marketing-automation-saas-zqmsmg/src/backend/apps/podcasts/__init__.py

---
+++ src/backend/apps/podcasts/__init__.py
@@ -0,0 +1,13 @@
+# It allows Python to import the podcasts app and its modules.
+
+# If any app-specific initialization is required in the future, it can be added here.
+# For example, you might want to set up app configurations or import signals.
+
+# from django.apps import AppConfig
+
+# class PodcastsConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'apps.podcasts'
+
+#     def ready(self):
+#         import apps.podcasts.signals
