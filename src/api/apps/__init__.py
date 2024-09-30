92c2c5743940a1681b8fd29410ad9d868ef0f8db
Siddhant P. Pardeshi <siddhantpp@gmail.com>
2024-09-30 23:43:54 +0530

    Add public-samples/podcast-marketing-automation-saas-zqmsmg/src/api/apps/__init__.py

---
+++ src/api/apps/__init__.py
@@ -0,0 +1,11 @@
+# It allows the applications within this directory to be imported elsewhere in the project.
+
+# The presence of this file enables Django to recognize the 'apps' directory
+# as a package, facilitating the organization of the project into smaller,
+# reusable applications.
+
+# Each subdirectory within 'apps' typically represents a distinct Django application,
+# each with its own models, views, and other related files.
+
+# By structuring the project this way, we promote modularity and separation of concerns,
+# making the codebase more maintainable and scalable.
